6ad878c81f5ce04ba373070975cea24c4ffc51c2
Grace Choi <gracec@zillowgroup.com>
2020-10-15 21:40:23 -0700

    merry/ray/brandon fixed the issue

--- server.js
+++ server.js
@@ -2,10 +2,11 @@
 'use strict';
 
 // dependencies
+require('dotenv').config();
+
 const express = require('express');
 const cors= require('cors');
 
-require('dotenv').config();
 
 // declare port for server to listen on 
 const PORT = process.env.PORT || 3000;
@@ -16,36 +17,46 @@ const app = express();
 // use cors
 app.use(cors());
 
+// test route
+// app.get('/testing', (request, response) => {
+//     console.log('Im here.');
+//     const test = {test: `this works on PORT${PORT}`}
+//     response.send(test);
+//   });
+
 // routes
 app.get('/location', (request, response) => {
     let city = request.query.city;
     let data = require('./data/location.json')[0];
     let location = new Location(data, city);
+    console.log(location);
     response.send(location);
     // console.log(request);
 });
 
 app.get('/weather', (request, response) => {
     let data = require('./data/weather.json');
+    data = data.data;
     let weatherArray = [];
     data.forEach(value => {
         let weather = new Weather(value);
         weatherArray.push(weather);
     });
+    console.log(weatherArray);
     response.send(weatherArray);
 });
 
 // constructor
 function Location(obj, query) {
-    this.lat = obj.lat;
-    this.lon = obj.lon;
+    this.latitude = obj.lat;
+    this.longitude = obj.lon;
     this.search_query = query;
-    this.location = obj.display_name;
+    this.formatted_query = obj.display_name;
 }
 
 function Weather(obj) {
-    this.description = obj.weather.description;
-    this.date = obj.valid_date;
+    this.forecast = obj.weather.description;
+    this.time = obj.valid_date;
 }
 
 // start server
