39f7b2a250ce86a264429ae306acd19da8fb84bc
Grace Choi <gracec@zillowgroup.com>
2020-10-17 14:32:30 -0700

    feature 2 done

--- server.js
+++ server.js
@@ -5,8 +5,8 @@
 require('dotenv').config();
 
 const express = require('express');
-const cors = require('cors');
 const superagent = require('superagent');
+const cors = require('cors');
 
 
 // declare port for server to listen on
@@ -24,43 +24,42 @@ app.use(cors());
 app.get('/location', locationHandler);
 function locationHandler(req, res) {
     let city = req.query.city;
-    let key = PROCESS.ENV.GEOCODE_API_KEY;
+    let key = process.env.GEOCODE_API_KEY;
 
-    const URL = //url
+    const URL = `https://us1.locationiq.com/v1/search.php/?key=${key}&q=${city}&format=json`;
 
     superagent.get(URL)
         .then(data => {
             console.log(data.body[0]);
-            let location = new Location (city, data.body[0]);
+            let location = new Location (data.body[0], city);
             res.status(200).json(location);
-        });
+        })
         .catch ((error) => {
             console.log('ERROR', error);
             res.status(500).send('Yikes. Something went wrong.');
-        })
+        });
 }
 
 
 app.get('/weather', weatherHandler);
 function weatherHandler(req, res) {
-    try {
-        let data = require('./data/weather.json');
-        data = data.data;
-        // let weatherArray = [];
-        // data.forEach(value => {
-        //     let weather = new Weather(value);
-        //     weatherArray.push(weather);
-        // });
-        // res.send(weatherArray);
-        let weather = data.map(value => {
-            return new Weather(value);
+    let key = process.env.WEATHER_API_KEY;
+    let lat = req.query.latitude;
+    let lon = req.query.longitude;
+
+    const URL = `https://api.weatherbit.io/v2.0/forecast/daily?lat=${lat}&lon=${lon}&key=${key}`;
+
+    superagent.get(URL)
+        .then(data => {
+            let weather = data.body.data.map(value => {
+                return new Weather(value);
+            });
+            res.status(200).json(weather);
+        })
+        .catch ((error) => {
+            console.log('ERROR', error);
+            res.status(500).send('Yikes. Something went wrong.');
         });
-        res.send(weather);
-    }
-    catch (error){
-        console.log('ERROR', error);
-        res.status(500).send('Yikes. Something went wrong.');
-    }
 }
 
 // constructor
