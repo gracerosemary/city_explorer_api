9292dcb28c12582aa78e1d54b09c7141c83bd07c
Grace Choi <gracec@zillowgroup.com>
2020-10-21 22:41:30 -0700

    feature 1 done

--- server.js
+++ server.js
@@ -3,11 +3,11 @@
 
 // dependencies
 require('dotenv').config();
-
 const express = require('express');
 const superagent = require('superagent');
 const cors = require('cors');
-
+const pg = require('pg');
+const e = require('express');
 
 // declare port for server to listen on
 const PORT = process.env.PORT || 3000;
@@ -18,26 +18,43 @@ const app = express();
 // use cors
 app.use(cors());
 
+// create our postgres client
+const client = new pg.Client(process.env.DATABASE_URL);
+
 // routes
-// app.use('*', notFoundHandler);
+app.use('*', notFoundHandler);
 
 app.get('/location', locationHandler);
 function locationHandler(req, res) {
     let city = req.query.city;
     let key = process.env.GEOCODE_API_KEY;
-
     const URL = `https://us1.locationiq.com/v1/search.php/?key=${key}&q=${city}&format=json`;
 
-    superagent.get(URL)
+    // insert sql syntax to select the city taken from the query (NEED TO DO!)
+    let SQL = `SELECT * FROM WHERE etc - only one value($1)`;
+    let safeValues = [city];
+
+    client.query(SQL, safeValues)
         .then(data => {
-            console.log(data.body[0]);
-            let location = new Location (data.body[0], city);
-            res.status(200).json(location);
+            // console.log(data);
+
+            // if statement - check the database for location information FIRST and send the location object in the response to the client (NEED TO DO!)
+            if (data ---- need to see if data has rows) {
+            res.status(200).json(data.rows);
+            // else statement - request data from API only if it does not exist in database (NEED TO DO!)
+            } else {
+                superagent.get(URL)
+                    .then(data => {
+                        // console.log(data.body[0]);
+                        let location = new Location (data.body[0], city);
+                        res.status(200).json(location);
+                    })
+                    .catch ((error) => {
+                        console.log('ERROR', error);
+                        res.status(500).send('Yikes. Something went wrong.');
+                    });
+            }
         })
-        .catch ((error) => {
-            console.log('ERROR', error);
-            res.status(500).send('Yikes. Something went wrong.');
-        });
 }
 
 
@@ -70,7 +87,7 @@ function trailHandler(req, res) {
 
 
     const URL = `https://www.hikingproject.com/data/get-trails?lat=${lat}&lon=${lon}&maxDistance=10&key=${key}`;
-    
+
     superagent.get(URL)
     .then(data => {
         console.log(data.body.trails);
@@ -111,9 +128,9 @@ function Trail(obj) {
     this.condition_time = obj.conditionDate.slice(11,20);
 }
 
-// function notFoundHandler(req, res) {
-//     res.status(404).send('huh?');
-// }
+function notFoundHandler(req, res) {
+    res.status(404).send('huh?');
+}
 
 // start server
 app.listen(PORT, () => {
